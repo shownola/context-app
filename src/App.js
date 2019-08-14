3f24a889c3c8c0924bc9672e4eb2548fc2749c60
shownola <swasieko@gmail.com>
2019-08-14 14:59:50 -0700

    Add context to app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Form from './Form';
+import PageContent from './PageContent';
+import {ThemeProvider} from './contexts/ThemeContext'
 
 
 function App() {
   return (
-    <>
-    <Navbar />
-    <Form />
-    </>
+    <ThemeProvider>
+      <PageContent>
+      <Navbar />
+      <Form />
+      </PageContent>
+    </ThemeProvider>
   );
 }
 

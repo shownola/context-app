b291a1ecf659cddda3751da6ec82b039c4e6294e
shownola <swasieko@gmail.com>
2019-08-14 14:34:39 -0700

    Add responsive form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './Navbar';
+import Form from './Form';
 
 
 function App() {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <>
+    <Navbar />
+    <Form />
+    </>
   );
 }
 
